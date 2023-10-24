366af1be2a741406f85b852b44e7851d4c3376ef
Mr. Jigglesworth <jesster_07@hotmail.com>
2023-10-24 14:54:28 -0500

    Updates for backup and restore

---
+++ Models/WalletInfo.cs
@@ -0,0 +1,7 @@
+public class WalletInfo
+{
+    // Define the structure of wallet data to be backed up
+    public string MnemonicPhrase { get; set; }
+    public string PrivateKeys { get; set; }
+    // Add other relevant wallet data
+}
